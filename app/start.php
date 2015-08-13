97d2330312d10f03260d724476e0b48683a9fa1b
Kevin Korte <kevinkorte15@gmail.com>
2015-08-13 23:54:15 +0000

    Config settings

--- app/start.php
+++ app/start.php
@@ -1,6 +1,7 @@
 <?php
 
 use Slim\Slim;
+use Noodlehaus\Config;
 
 session_cache_limiter(false);
 session_start();
@@ -10,5 +11,12 @@ ini_set('display_errors', 'On');
 define ('INC_ROOT', dirname(__DIR__));
 
 require INC_ROOT . '/vendor/autoload.php';
+require '../../secrets/secrets.php';
 
-$app = new Slim();
+$app = new Slim([
+    'mode' => file_get_contents(INC_ROOT . '/mode.php')
+]);
+
+$app->configureMode($app->config('mode'), function() use ($app) {
+    $app->config = Config::load(INC_ROOT . "/app/config/{$app->mode}.php");
+});
