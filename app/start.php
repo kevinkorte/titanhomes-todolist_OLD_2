0a98cf0a58c5a2deaf0092bfb287ba356283a19f
Kevin Korte <kevinkorte15@gmail.com>
2015-08-18 15:17:32 +0000

    Created routes and views with Twig

--- app/start.php
+++ app/start.php
@@ -1,6 +1,9 @@
 <?php
 
 use Slim\Slim;
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
+
 use Noodlehaus\Config;
 
 use Titanhomes\User\User;
@@ -16,7 +19,9 @@ require INC_ROOT . '/vendor/autoload.php';
 require '../../secrets/secrets.php';
 
 $app = new Slim([
-    'mode' => file_get_contents(INC_ROOT . '/mode.php')
+    'mode' => file_get_contents(INC_ROOT . '/mode.php'),
+    'view' => new Twig(),
+    'templates.path' => INC_ROOT . '/app/views'
 ]);
 
 $app->configureMode($app->config('mode'), function() use ($app) {
@@ -24,7 +29,18 @@ $app->configureMode($app->config('mode'), function() use ($app) {
 });
 
 require 'database.php';
+require 'routes.php';
 
 $app->container->set('user', function() {
     return new User;
 });
+
+$view = $app->view();
+
+$view->parserOptions = [
+    'debug' => $app->config->get('twig.debug')
+];
+
+$view->parserExtensions = [
+    new TwigExtension
+];
